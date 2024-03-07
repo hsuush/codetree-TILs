c916ab95c3004954732995e9e1e47fc83aeedd35
이혜수 <119154537+hsuush@users.noreply.github.com>
2024-03-07 23:19:43 +0900

    Title: 각 자리 숫자의 제곱, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240307/각 자리 숫자의 제곱/square-of-each-digit.java
+++ 240307/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -1,14 +1,19 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int num = sc.nextInt();
+        int num = Integer.parseInt(st.nextToken());
 
-        double res = add(num);
-        System.out.print((int)res);
+        int res = (int)add(num);
+        sb.append(res);
+
+        System.out.print(sb);
 
     }
 
