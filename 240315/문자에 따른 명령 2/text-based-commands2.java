377e96de11b8b5dccd65bea5082ac3b00851e09a
이혜수 <119154537+hsuush@users.noreply.github.com>
2024-03-15 19:35:23 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240315/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main { ;
+
+
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        int[] dx = new int[]{1, 0, -1, 0};
+        int[] dy = new int[]{0,-1,0,1};
+
+        int x = 0, y = 0;
+        int dirNum = 3;
+
+        String str = sc.next();
+
+        for(int i = 0; i < str.length(); i++){
+            char method = str.charAt(i);
+
+            switch(method){
+                case 'R':
+                    dirNum = (dirNum + 1) % 4;
+                    break;
+                case 'L':
+                    dirNum = (dirNum - 1) % 4;
+                    break;
+                case 'F':
+                    x += dx[dirNum];
+                    y += dy[dirNum];
+                    break;
+            }
+        }
+
+        System.out.println(x + " " + y);
+
+
+    }
+
+}
